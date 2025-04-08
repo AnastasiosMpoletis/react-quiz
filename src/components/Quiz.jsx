191227a77653e5de8ad97c93d8fba40b217e9003
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-08 21:06:33 +0300

    L-195. Display questions, answers and navigate between questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,34 @@
 import { useState } from "react";
+import QUESTIONS from '../questions.js'
 
 export default function Quiz() {
-    const [activeQuestion, setActiveQuestion] = useState(0);
     const [userAnswers, setUserAnswers] = useState([]);
 
+    /**
+     * If there is an answer for a question then the active question is the next one.
+     */
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers(previousUserAnswers => {
+            return [...previousUserAnswers, selectedAnswer];
+        });
+    }
+
     return (
-        <>
-            <p>Question</p>
-        </>
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                        <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}>
+                                {answer}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     );
 }
